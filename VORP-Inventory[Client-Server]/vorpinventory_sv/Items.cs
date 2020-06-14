47a300012d9d5edd212f8c5081c5fb190ba2cb5d
Avililla <54501703+Avililla@users.noreply.github.com>
2020-06-14 18:30:22 +0200

    Update items type on server resolving problems when using them

--- VORP-Inventory[Client-Server]/vorpinventory_sv/Items.cs
+++ VORP-Inventory[Client-Server]/vorpinventory_sv/Items.cs
@@ -6,13 +6,25 @@
         private string label;
         private int limit;
         private bool can_remove;
+        private string type;
 
-        public Items(string item, string label, int limit, bool can_remove)
+        public Items(string item, string label, int limit, bool can_remove,string type)
         {
             this.item = item;
             this.limit = limit;
             this.can_remove = can_remove;
             this.label = label;
+            this.type = type;
+        }
+
+        public string getType()
+        {
+            return this.type;
+        }
+
+        public void setType(string type)
+        {
+            this.type = type;
         }
 
         public string getName()
